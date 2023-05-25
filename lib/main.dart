f5f0e849877c9c7c268eb471bbc5749af6fb0df0
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-25 23:24:32 +0530

    Created BaseObject class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
@@ -13,6 +14,29 @@ void main() {
   );
 }
 
+/// Immutable class [BaseObject] which will be inherited later
+@immutable
+class BaseObject {
+  /// Unique identifier for objects
+  final String id;
+
+  /// Last updated time for objects
+  final String lastUpdated;
+
+  /// Default constructor for initializing parameters [id] and [lastUpdated]
+  BaseObject()
+      : id = const Uuid().v4(),
+        lastUpdated = DateTime.now().toIso8601String();
+
+  /// Overriding [==] operator to compare ids
+  @override
+  bool operator ==(covariant BaseObject other) => id == other.id;
+
+  /// Overriding [hashCode] getter to get hashCode of [id] instead.
+  @override
+  int get hashCode => id.hashCode;
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -20,7 +44,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Center(child: Text('Home Page')),
       ),
     );
   }
